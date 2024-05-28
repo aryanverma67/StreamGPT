3f55052996d2f363c6b9ec45fce9cce7e7ce2b27
Aryan Verma <145522900+aryanverma67@users.noreply.github.com>
2024-05-28 14:14:51 +0530

    some minor changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 
 export const BACKGROUND_IMAGE = "https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
 
-export const USER_AVATAR = "../public/user_avtar.png";
+export const USER_AVATAR = "https://www.clipartkey.com/mpngs/m/80-809592_transparent-profile-clipart-user-vector-icon-png.png";
 
 
 
