931efa9dc8ddb7e66a09e3b99c89131b3aea9bb4
Aryan Verma <145522900+aryanverma67@users.noreply.github.com>
2024-06-29 10:04:46 +0530

    new

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,7 +2,7 @@ import {configureStore} from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import moviesReducer from "./slices/movieSlice";
 import gptReducer from"./slices/Gptslice";
-import configReducer from "./slices/ConfigSlice"
+import configReducer from "./slices/ConfigSlice.js"
 
 const appStore = configureStore(
     {
